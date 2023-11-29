65fbabbb4a3fe01c91f11d840303a3aba059764f
Henrique3040 <henrique.de.sousa.lima.cardoso@student.ehb.be>
2023-11-29 16:33:57 +0100

    Maken_bot

---
+++ Bot/src/main/java/org/example/Bot.java
@@ -0,0 +1,28 @@
+package org.example;
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import reactor.core.publisher.Mono;
+public class Bot {
+    public static void main(String[] args) {
+
+        DiscordClient client = DiscordClient.create("MTE3ODMwNjQ0NDI5NTE2MzkyNQ.GOGYlz.j1hHfbo_chStbkJKnvUcoDxPK-9woRRImS4GoE");
+
+        // This part of the code determines which messages in the chat to listen to
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
+                gateway.on(MessageCreateEvent.class, event -> {
+                    Message message = event.getMessage();
+
+                    if (message.getContent().equalsIgnoreCase("!ping")) {
+                        return message.getChannel()
+                                .flatMap(channel -> channel.createMessage("pong!"));
+                    }
+
+                    return Mono.empty();
+                }));
+
+        login.block();
+
+    }
+}
