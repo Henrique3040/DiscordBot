d61d9b506f5aa9d396c8a363f2174e45bc3390aa
Henrique3040 <henrique.de.sousa.lima.cardoso@student.ehb.be>
2023-11-30 17:50:46 +0100

    Laatste_Try_geen_succes

---
+++ Bot/src/main/java/org/example/Command.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import reactor.core.publisher.Mono;
+
+public interface Command {
+    Mono<Void> execute(MessageCreateEvent event);
+}
